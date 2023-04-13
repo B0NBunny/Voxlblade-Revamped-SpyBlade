c02514a2b027ce2edb3047b25cddb2af2caacffa
B0NBunny <69701156+B0NBunny@users.noreply.github.com>
2023-04-13 00:07:04 -0400

    Update SpyBlade_loader.lua

--- SpyBlade_loader.lua
+++ SpyBlade_loader.lua
@@ -4,26 +4,30 @@ KeybindCode = Enum.KeyCode.Period
 DarkMode = true
 
 ESP_Coloring = {
-  Entities = {
-    Players = BrickColor.new("White").Color;
-    NPCs = BrickColor.new("Lime green").Color;
-    Enemies = {
+  Players = BrickColor.new("White").Color;
+  
+  Enemies = {
       Legendary = BrickColor.new("Bright yellow").Color;
       Magical = BrickColor.new("Cyan").Color;
       Corrupt = BrickColor.new("Bright purple").Color;
       Bloody = BrickColor.new("Maroon").Color;
       Easter = BrickColor.new("Pastel violet").Color;
       Other = BrickColor.new("Really red").Color;
-    };
   };
-  Items = BrickColor.new("Magenta").Color;
-  Shrines = BrickColor.new("Medium Royal blue").Color;
+  
+  NPCs = BrickColor.new("Lime green").Color;
+  BloodHands = BrickColor.new("Lime green").Color;
+
+  Shops = BrickColor.new("Pastel blue-green").Color;
+  Crafting = BrickColor.new("Pastel blue-green").Color;
+  Anvils = BrickColor.new("Pastel blue-green").Color;
   Infusers = BrickColor.new("Pastel blue-green").Color;
+  
   Dungeons = BrickColor.new("CGA brown").Color;
-  VoidRifts = BrickColor.new("Dark indigo").Color;
+  VoidRifts = BrickColor.new("CGA brown").Color;
+  
+  Shrines = BrickColor.new("Medium Royal blue").Color;
 }
-
-
 
 local ScriptLink = "https://github.com/B0NBunny/Voxlblade-2-Scripts/blob/main/SpyBlade.lua"
 loadstring(game:HttpGet(ScriptLink, true))()
