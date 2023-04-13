85af717c37585d3b100f672011c6f15b39f91626
B0NBunny <69701156+B0NBunny@users.noreply.github.com>
2023-04-13 17:48:14 -0400

    Update SpyBlade_loader.lua

--- SpyBlade_loader.lua
+++ SpyBlade_loader.lua
@@ -3,32 +3,31 @@ Keybind = "Period"
 KeybindCode = Enum.KeyCode.Period
 DarkMode = true
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/B0NBunny/Voxlblade-2-Scripts/main/SpyBlade.lua", true))()
 
 ESP_Coloring = {
-  Players = BrickColor.new("White").Color,
+  Players = BrickColor.new("White").Color;
 
   Enemies = {
-    Legendary = BrickColor.new("Bright yellow").Color,
-    Magical = BrickColor.new("Cyan").Color,
-    Corrupt = BrickColor.new("Bright purple").Color,
-    Bloody = BrickColor.new("Maroon").Color,
+    Legendary = BrickColor.new("Bright yellow").Color;
+    Magical = BrickColor.new("Cyan").Color;
+    Corrupt = BrickColor.new("Bright purple").Color;
+    Bloody = BrickColor.new("Maroon").Color;
     Other = BrickColor.new("Really red").Color
-  },
+  };
 
-  NPCs = BrickColor.new("Lime green").Color,
-  BloodHands = BrickColor.new("Lime green").Color,
+  NPCs = BrickColor.new("Lime green").Color;
+  BloodHands = BrickColor.new("Lime green").Color;
 
-  Shops = BrickColor.new("Pastel blue-green").Color,
-  Craftings = BrickColor.new("Pastel blue-green").Color,
-  Anvils = BrickColor.new("Pastel blue-green").Color,
-  Infusers = BrickColor.new("Pastel blue-green").Color,
-  Transfers = BrickColor.new("Pastel blue-green").Color,
+  Shops = BrickColor.new("Pastel blue-green").Color;
+  Craftings = BrickColor.new("Pastel blue-green").Color;
+  Anvils = BrickColor.new("Pastel blue-green").Color;
+  Infusers = BrickColor.new("Pastel blue-green").Color;
+  Transfers = BrickColor.new("Pastel blue-green").Color;
 
-  Dungeons = BrickColor.new("CGA brown").Color,
-  VoidRifts = BrickColor.new("CGA brown").Color,
+  Dungeons = BrickColor.new("CGA brown").Color;
+  VoidRifts = BrickColor.new("CGA brown").Color;
 
-  Shrines = BrickColor.new("Medium Royal blue").Color
+  Shrines = BrickColor.new("Medium Royal blue").Color;
 }
 
-print("A")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/B0NBunny/Voxlblade-2-Scripts/main/SpyBlade.lua", true))()
