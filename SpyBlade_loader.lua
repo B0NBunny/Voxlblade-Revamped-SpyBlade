765e6ecc7514297c0658aef76967331dfcb2b16d
B0NBunny <69701156+B0NBunny@users.noreply.github.com>
2023-04-12 23:11:58 -0400

    Update and rename SpyBlade_loader to SpyBlade_loader.lua

---
+++ SpyBlade_loader.lua
@@ -0,0 +1,21 @@
+--Voxlblade Item/Enemy/NPC ESP by Brycki
+Keybind = "Period"
+KeybindCode = Enum.KeyCode.Period
+DarkMode = true
+
+ESP_Coloring = {
+  Entities = {
+    Players = BrickColor.new("White").Color;
+    NPCs = BrickColor.new("Lime green").Color;
+    Enemies = {
+      Legendary = BrickColor.new("Bright yellow").Color;
+      Magical = BrickColor.new("Cyan").Color;
+      Corrupt = BrickColor.new("Lavender").Color;
+      Bloody = BrickColor.new("Maroon").Color;
+      Easter = BrickColor.new("Pastel violet").Color;
+      Other = BrickColor.new("Really red").Color;
+    };
+  };
+  Items = BrickColor.new("Magenta").Color;
+  Shrines = BrickColor.new("Pastel Blue").Color;
+}
