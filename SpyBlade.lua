acc726db09b6464c3c81edb4b8c0e625b2986f3e
B0NBunny <69701156+B0NBunny@users.noreply.github.com>
2023-04-14 22:52:37 -0400

    Update SpyBlade.lua

--- SpyBlade.lua
+++ SpyBlade.lua
@@ -95,7 +95,7 @@ local FinityWindow = Finity.new("SpyBlade", DarkMode, false, "", false, "")
 Args:
 Title (String), DarkMode (boolean), UseCustomTheme (boolean), CustomThemeName (String), HideToolTip (boolean), ToolTip (String)
 ]]
-FinityWindow.ChangeToggleKey(KeybindCode or Enum.KeyCode.Period)
+FinityWindow.ChangeToggleKey(Keybind)
 
 local CreditsCategory = FinityWindow:Category("Credits");
 local CreditsSector = CreditsCategory:Sector("Credits");
